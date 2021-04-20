bfa993d3a15c0306e4001750279dc02355c68944
lppgn <33622086+lppgn@users.noreply.github.com>
2021-04-20 13:18:01 +0200

    Klasser färdiga

---
+++ Slutprojekt/Player.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Slutprojekt
+{
+    public class Player
+    {
+        static Random dice = new Random();
+    }
+}
---
+++ Slutprojekt/propertySquare.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Slutprojekt
+{
+    public class propertySquare
+    {
+        
+    }
+}
