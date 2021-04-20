2d19349a554dbcc49749278fe6f01759babea07e
lppgn <33622086+lppgn@users.noreply.github.com>
2021-04-20 13:34:19 +0200

    dag 1 färdig

--- Slutprojekt/Player.cs
+++ Slutprojekt/Player.cs
@@ -1,9 +1,13 @@
 using System;
-
+using System.Collections.Generic;
 namespace Slutprojekt
 {
     public class Player
     {
         static Random dice = new Random();
+        public int position = 0;
+        public int cash = 1000;
+        public int prisonCard = 0;
+        public Dictionary<int, boardSquare> boardSquaresOwned = new Dictionary<int, boardSquare>();
     }
 }
