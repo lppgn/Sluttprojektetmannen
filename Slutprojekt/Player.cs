1b3d521ae53b0a84da865ba630f2c62c00acc505
lppgn <33622086+lppgn@users.noreply.github.com>
2021-04-22 15:29:24 +0200

    Hur man skapar players

--- Slutprojekt/Player.cs
+++ Slutprojekt/Player.cs
@@ -5,9 +5,17 @@ namespace Slutprojekt
     public class Player
     {
         static Random dice = new Random();
+        static Dictionary<int, Player> players = new Dictionary<int, Player>();
+        public string Name = "";
         public int position = 0;
         public int cash = 1000;
         public int prisonCard = 0;
         public Dictionary<int, boardSquare> boardSquaresOwned = new Dictionary<int, boardSquare>();
+
+        public Player(string name, int i)
+        {
+            this.Name = name;
+            players.Add(i, this);
+        }
     }
 }
